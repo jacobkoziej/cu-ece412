5228e22d24236d08e5fe22c81f0c8c611678f885
Jacob Koziej <jacobkoziej@gmail.com>
2024-10-07 16:33:38 -0400

    Load LibriSpeech ASR corpus

---
+++ whisper/notebook.py
@@ -0,0 +1,41 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# noteboook.py -- OpenAI's Whisper on LibriSpeech ASR
+# Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+# %%
+import os
+
+from torchaudio.datasets import LIBRISPEECH
+
+# %%
+train = LIBRISPEECH(
+    root=os.environ.get("DATASETS_PATH", "."),
+    url="train-clean-100",
+    download=True,
+)
+validate = LIBRISPEECH(
+    root=os.environ.get("DATASETS_PATH", "."),
+    url="dev-clean",
+    download=True,
+)
+test = LIBRISPEECH(
+    root=os.environ.get("DATASETS_PATH", "."),
+    url="test-clean",
+    download=True,
+)
