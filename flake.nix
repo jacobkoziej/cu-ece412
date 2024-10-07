beee6b1f98a088f8a57fc643f7e758e134db0eb2
Jacob Koziej <jacobkoziej@gmail.com>
2024-10-07 13:34:13 -0400

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "The Cooper Union - ECE 412: Speech and Audio Signal Processing";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs:
+
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+
+        python = pkgs.python3;
+
+        python-pkgs = python.withPackages (
+          python-pkgs: with python-pkgs; [
+            ipython
+            jupyter
+            jupytext
+            papermill
+          ]
+        );
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages =
+            [
+              python-pkgs
+            ]
+            ++ (with pkgs; [
+              black
+              scons
+              treefmt2
+            ]);
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
