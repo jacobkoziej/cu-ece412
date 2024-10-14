4e7c58e0f0e6ce430236a080e5d4ad402bb16d95
Jacob Koziej <jacobkoziej@gmail.com>
2024-10-13 22:13:04 -0400

    Move functionality to `whisper_wrappers.py`

--- whisper/train.py
+++ whisper/train.py
@@ -10,143 +10,22 @@ import argparse
 import torch
 
 from einops import rearrange
-from jiwer import (
-    cer,
-    wer,
-)
-from pytorch_lightning import (
-    LightningModule,
-    Trainer,
-)
+from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
-from torch import (
-    nn,
-    optim,
-)
-from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
-from whisper.audio import log_mel_spectrogram
 from whisper.decoding import DecodingOptions
 from whisper.normalizers import EnglishTextNormalizer
 from whisper.tokenizer import get_tokenizer
 from whisper_wrappers import (
+    Collate,
     LibriSpeech,
+    Whisper,
     load_base_model,
 )
 
 
-IGNORE_INDEX = -100
 LANGUAGE = "en"
 MODEL = "tiny.en"
-
-
-class Collate:
-    def __init__(self, normalizer, tokenizer):
-        self.normalizer = normalizer
-        self.tokenizer = tokenizer
-
-    def __call__(self, batch):
-        mel = []
-        tokens = []
-
-        for audio, transcript in batch:
-            mel += [log_mel_spectrogram(audio)]
-            tokens += [
-                [*self.tokenizer.sot_sequence_including_notimestamps]
-                + self.tokenizer.encode(transcript)
-                + self.tokenizer.encode(self.normalizer(transcript))
-            ]
-
-        mel = torch.stack(mel)
-
-        labels = [torch.tensor(t[1:] + [self.tokenizer.eot]) for t in tokens]
-        labels = pad_sequence(
-            labels,
-            batch_first=True,
-            padding_value=IGNORE_INDEX,
-        )
-
-        tokens = pad_sequence(
-            [torch.tensor(t) for t in tokens],
-            batch_first=True,
-            padding_value=self.tokenizer.eot,
-        )
-
-        return mel, tokens, labels
-
-
-class WhisperFineTuner(LightningModule):
-    def __init__(self, model, options, normalizer, tokenizer):
-        super().__init__()
-
-        self.model = model
-        self.options = options
-        self.normalizer = normalizer
-        self.tokenizer = tokenizer
-
-        self.loss = nn.CrossEntropyLoss(ignore_index=IGNORE_INDEX)
-        self.cer = cer
-        self.wer = wer
-
-        for p in self.model.parameters():
-            p.requires_grad = False
-
-        for p in self.model.decoder.parameters():
-            p.requires_grad = True
-
-    def _decode(self, tokens):
-        tokens[tokens == IGNORE_INDEX] = self.tokenizer.eot
-
-        transcripts = [self.tokenizer.decode(t).strip() for t in tokens]
-        transcripts = [self.normalizer(t) for t in transcripts]
-
-        return transcripts
-
-    def _rearrange(self, prediction, labels):
-        prediction = rearrange(prediction, "b t f -> (b t) f")
-        labels = rearrange(labels, "b t -> (b t)")
-
-        return prediction, labels
-
-    def training_step(self, batch, batch_idx):
-        mel, tokens, labels = batch
-
-        prediction = self.model.forward(mel, tokens)
-
-        prediction, labels = self._rearrange(prediction, labels)
-
-        loss = self.loss(prediction, labels)
-
-        self.log("train/loss", loss, prog_bar=True)
-
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        mel, tokens, labels = batch
-
-        prediction = self.model.forward(mel, tokens)
-
-        reference = self._decode(torch.argmax(prediction, axis=-1))
-        hypothesis = self._decode(labels)
-
-        cer = self.cer(reference, hypothesis)
-        wer = self.wer(reference, hypothesis)
-
-        prediction, labels = self._rearrange(prediction, labels)
-
-        loss = self.loss(prediction, labels)
-
-        self.log("val/loss", loss, prog_bar=True)
-        self.log("val/cer", cer, prog_bar=True)
-        self.log("val/wer", wer, prog_bar=True)
-
-    def configure_optimizers(self):
-        optimizer = optim.Adam(
-            self.parameters(),
-            lr=1e-6,
-        )
-
-        return optimizer
 
 
 def main() -> None:
@@ -211,7 +90,7 @@ def main() -> None:
         num_workers=torch.cuda.device_count() * 4,
     )
 
-    model = WhisperFineTuner(
+    model = Whisper(
         load_base_model(MODEL),
         options,
         normalizer,
--- whisper/whisper_wrappers.py
+++ whisper/whisper_wrappers.py
@@ -7,17 +7,69 @@
 import os
 
 import torch
+import whisper
 
+from einops import rearrange
+from jiwer import (
+    cer,
+    wer,
+)
+from pytorch_lightning import LightningModule
+from torch import (
+    nn,
+    optim,
+)
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
 from torchaudio.datasets import LIBRISPEECH
 from whisper import (
     ModelDimensions,
-    Whisper,
     _ALIGNMENT_HEADS,
     _MODELS,
     _download,
 )
-from whisper.audio import pad_or_trim
+from whisper.audio import (
+    pad_or_trim,
+    log_mel_spectrogram,
+)
+
+
+IGNORE_INDEX = -100
+
+
+class Collate:
+    def __init__(self, normalizer, tokenizer):
+        self.normalizer = normalizer
+        self.tokenizer = tokenizer
+
+    def __call__(self, batch):
+        mel = []
+        tokens = []
+
+        for audio, transcript in batch:
+            mel += [log_mel_spectrogram(audio)]
+            tokens += [
+                [*self.tokenizer.sot_sequence_including_notimestamps]
+                + self.tokenizer.encode(transcript)
+                + self.tokenizer.encode(self.normalizer(transcript))
+            ]
+
+        mel = torch.stack(mel)
+
+        labels = [torch.tensor(t[1:] + [self.tokenizer.eot]) for t in tokens]
+        labels = pad_sequence(
+            labels,
+            batch_first=True,
+            padding_value=IGNORE_INDEX,
+        )
+
+        tokens = pad_sequence(
+            [torch.tensor(t) for t in tokens],
+            batch_first=True,
+            padding_value=self.tokenizer.eot,
+        )
+
+        return mel, tokens, labels
 
 
 class LibriSpeech(Dataset):
@@ -39,6 +91,80 @@ class LibriSpeech(Dataset):
         return audio, transcript
 
 
+class Whisper(LightningModule):
+    def __init__(self, model, options, normalizer, tokenizer):
+        super().__init__()
+
+        self.model = model
+        self.options = options
+        self.normalizer = normalizer
+        self.tokenizer = tokenizer
+
+        self.loss = nn.CrossEntropyLoss(ignore_index=IGNORE_INDEX)
+        self.cer = cer
+        self.wer = wer
+
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+        for p in self.model.decoder.parameters():
+            p.requires_grad = True
+
+    def _decode(self, tokens):
+        tokens[tokens == IGNORE_INDEX] = self.tokenizer.eot
+
+        transcripts = [self.tokenizer.decode(t).strip() for t in tokens]
+        transcripts = [self.normalizer(t) for t in transcripts]
+
+        return transcripts
+
+    def _rearrange(self, prediction, labels):
+        prediction = rearrange(prediction, "b t f -> (b t) f")
+        labels = rearrange(labels, "b t -> (b t)")
+
+        return prediction, labels
+
+    def training_step(self, batch, batch_idx):
+        mel, tokens, labels = batch
+
+        prediction = self.model.forward(mel, tokens)
+
+        prediction, labels = self._rearrange(prediction, labels)
+
+        loss = self.loss(prediction, labels)
+
+        self.log("train/loss", loss, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        mel, tokens, labels = batch
+
+        prediction = self.model.forward(mel, tokens)
+
+        reference = self._decode(torch.argmax(prediction, axis=-1))
+        hypothesis = self._decode(labels)
+
+        cer = self.cer(reference, hypothesis)
+        wer = self.wer(reference, hypothesis)
+
+        prediction, labels = self._rearrange(prediction, labels)
+
+        loss = self.loss(prediction, labels)
+
+        self.log("val/loss", loss, prog_bar=True)
+        self.log("val/cer", cer, prog_bar=True)
+        self.log("val/wer", wer, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(
+            self.parameters(),
+            lr=1e-6,
+        )
+
+        return optimizer
+
+
 def load_base_model(name: str) -> Whisper:
     default = os.path.join(os.path.expanduser("~"), ".cache")
     download_root = os.path.join(os.getenv("XDG_CACHE_HOME", default), "whisper")
@@ -53,7 +179,7 @@ def load_base_model(name: str) -> Whisper:
 
     dims = ModelDimensions(**checkpoint["dims"])
 
-    model = Whisper(dims)
+    model = whisper.Whisper(dims)
 
     model.load_state_dict(checkpoint["model_state_dict"])
     model.set_alignment_heads(alignment_heads)
