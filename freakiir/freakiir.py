0280383cabb664e6c1332292741e856128058b98
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-01 17:15:20 -0400

    Add `freakiir.py`
    co-authored-by: Eric Eng <eric.eng@cooper.edu>

---
+++ freakiir/freakiir.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# freakiir.py -- freakIIR: IIRNet but with phase
+# Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+# Copyright (C) 2024  Eric Eng <eric.eng@cooper.edu>
