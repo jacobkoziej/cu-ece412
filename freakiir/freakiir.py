3e0a0ab4dce35e426d7443ae15ba3e35b6dcad2c
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-01 23:02:53 -0400

    Define `dft2input()`

--- freakiir/freakiir.py
+++ freakiir/freakiir.py
@@ -3,3 +3,14 @@
 # freakiir.py -- freakIIR: IIRNet but with phase
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
 # Copyright (C) 2024  Eric Eng <eric.eng@cooper.edu>
+
+import torch
+
+from einops import rearrange
+
+
+def dft2input(f: torch.Tensor):
+    z = torch.log10(f)
+    z = torch.stack([10 * z.real, z.imag], axis=-1)
+
+    return rearrange(z, "... w z -> ... (w z)")
