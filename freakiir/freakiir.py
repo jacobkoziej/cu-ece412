59b18fc4e0abb1fd535972dcbd704ba71754d5bb
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-01 23:06:47 -0400

    Define `FreakIirDataset`
    co-authored-by: Eric Eng <eric.eng@cooper.edu>

--- freakiir/freakiir.py
+++ freakiir/freakiir.py
@@ -4,9 +4,53 @@
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
 # Copyright (C) 2024  Eric Eng <eric.eng@cooper.edu>
 
+import numpy as np
 import torch
 
-from einops import rearrange
+from einops import (
+    rearrange,
+    reduce,
+)
+from scipy.signal import freqz_zpk
+from torch.utils.data import Dataset
+
+
+class FreakIirDataset(Dataset):
+    def __init__(self, dataset: torch.Tensor, order: int, *, N: int = 512):
+        assert order >= 2
+        assert not order % 2
+
+        self.riemann_sphere = rearrange(
+            dataset, "(batch order) h -> batch order h", order=order
+        )
+
+        zp = torch.exp(1j * dataset[..., ::2]) * (
+            1 / torch.tan(0.5 * dataset[..., 1::2])
+        )
+        zp = zp.numpy()
+
+        z = zp[..., :2]
+        p = zp[..., 2:]
+
+        h = torch.tensor(
+            np.array([freqz_zpk(z, p, 1, worN=N, whole=True)[-1] for z, p in zip(z, p)])
+        )
+        h = rearrange(h, "(batch order) h -> batch order h", order=order)
+        h = reduce(h, "batch order h -> batch h", "prod")
+
+        self.h = h
+
+        self.input = dft2input(h)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, item):
+        riemann_sphere = self.riemann_sphere[item]
+        h = self.h[item]
+        input = self.input[item]
+
+        return riemann_sphere, h, input
 
 
 def dft2input(f: torch.Tensor):
