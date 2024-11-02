485cde301ae9e2e35be7bfb0ddba00dd0eedef42
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-01 23:45:40 -0400

    Define `FreakIir`
    co-authored-by: Eric Eng <eric.eng@cooper.edu>

--- freakiir/freakiir.py
+++ freakiir/freakiir.py
@@ -11,8 +11,48 @@ from einops import (
     rearrange,
     reduce,
 )
+from pytorch_lightning import LightningModule
 from scipy.signal import freqz_zpk
+from torch import nn
 from torch.utils.data import Dataset
+
+
+class FreakIir(LightningModule):
+    def __init__(
+        self,
+        inputs: int = 1024,
+        layers: int = 2,
+        hidden_dimension: int = 16 * 1024,
+        order: int = 4,
+        *,
+        negative_slope: float = 0.2,
+    ):
+        assert layers >= 2
+
+        assert not inputs % 2
+        assert not order % 2
+
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.layers = nn.ModuleList()
+
+        layers -= 2
+
+        def gen_layer(in_dimension, out_dimension):
+            return nn.Sequential(
+                nn.Linear(in_dimension, out_dimension),
+                nn.LayerNorm(out_dimension),
+                nn.LeakyReLU(negative_slope),
+            )
+
+        self.layers.append(gen_layer(inputs, hidden_dimension))
+
+        for layer in range(layers):
+            self.layers.append(gen_layer(hidden_dimension, hidden_dimension))
+
+        self.layers.append(nn.Linear(hidden_dimension, order * 8))
 
 
 class FreakIirDataset(Dataset):
