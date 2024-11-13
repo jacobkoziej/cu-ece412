bdce0303d980e0b7eeaa980bf7d5f0e09389ee10
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-13 00:11:04 -0500

    Define `uniform_disk()`

--- freakiir/generate.py
+++ freakiir/generate.py
@@ -1,77 +1,31 @@
-#!/usr/bin/env python
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# generate.py -- generate points on the Riemann sphere
+# generate.py -- generate random filters
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
 # Copyright (C) 2024  Eric Eng <eric.eng@cooper.edu>
 
+import torch
 
-def main() -> None:
-    import numpy as np
-    import pandas as pd
-
-    from argparse import ArgumentParser
-    from pathlib import Path
-
-    argparser = ArgumentParser(
-        description="generate uniform points on the unit sphere",
-    )
-
-    argparser.add_argument(
-        "-e",
-        "--epsilon",
-        default=1e-8,
-        help="pole distance from unit circle [rad]",
-        type=float,
-    )
-    argparser.add_argument(
-        "-n",
-        "--points",
-        help="number of points to generate",
-        required=True,
-        type=int,
-    )
-    argparser.add_argument(
-        "-o",
-        "--output",
-        help="output name",
-        required=True,
-        type=Path,
-    )
-    argparser.add_argument(
-        "-s",
-        "--seed",
-        default=0x851F3CE1,
-        help="rng seed",
-        type=int,
-    )
-
-    args = argparser.parse_args()
-
-    theta_limits = (-np.pi, np.pi - np.finfo(np.pi).eps)
-    zero_phi_limits = (0, np.pi)
-    pole_phi_limits = ((np.pi / 2) + abs(args.epsilon), np.pi)
-
-    rng = np.random.default_rng(args.seed)
-
-    data = {
-        "zero_0_theta": rng.uniform(*theta_limits, args.points),
-        "zero_0_phi": rng.uniform(*zero_phi_limits, args.points),
-        "zero_1_theta": rng.uniform(*theta_limits, args.points),
-        "zero_1_phi": rng.uniform(*zero_phi_limits, args.points),
-        "pole_0_theta": rng.uniform(*theta_limits, args.points),
-        "pole_0_phi": rng.uniform(*pole_phi_limits, args.points),
-        "pole_1_theta": rng.uniform(*theta_limits, args.points),
-        "pole_1_phi": rng.uniform(*pole_phi_limits, args.points),
-    }
-
-    df = pd.DataFrame(data)
-
-    df.to_csv(f"{args.output}.csv", index=False)
-
-    with open(f"{args.output}.args", "w") as f:
-        f.write(f"{args}\n")
+from typing import Optional
 
 
-if __name__ == "__main__":
-    main()
+def uniform_disk(
+    pairs: int,
+    *,
+    epsilon: float = 1e-6,
+    dtype: Optional[torch.dtype] = None,
+) -> torch.Tensor:
+    from torch.distributions import Uniform
+
+    if dtype is None:
+        dtype = torch.get_default_dtype()
+
+    samples: tuple[int] = (pairs * 2,)
+
+    r_uniform: Uniform = Uniform(0 + epsilon, 1 - epsilon)
+    theta_uniform: Uniform = Uniform(0, 2 * torch.pi - torch.finfo(dtype).eps)
+
+    r: torch.Tensor = r_uniform.sample(samples)
+    theta: torch.Tensor = theta_uniform.sample(samples)
+
+    return r * torch.exp(1j * theta)
