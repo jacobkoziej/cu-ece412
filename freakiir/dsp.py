db86e3cf911a612f2a2192913efd162fd346bd9b
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-22 02:45:52 -0500

    Define `unwrap()`

--- freakiir/dsp.py
+++ freakiir/dsp.py
@@ -6,6 +6,7 @@
 import torch
 
 from einops import reduce
+from torch import pi
 
 
 def freqz_zpk(
@@ -22,7 +23,7 @@ def freqz_zpk(
 
     assert k.device == z.device
 
-    w = torch.linspace(0, torch.pi * (2 if whole else 1), N).to(z.device)
+    w = torch.linspace(0, pi * (2 if whole else 1), N).to(z.device)
     h = torch.exp(1j * w)
 
     k = k.reshape(k.shape + (1,) * (z.ndim - 1))
@@ -36,3 +37,9 @@ def polyvalfromroots(x: torch.Tensor, r: torch.Tensor) -> torch.Tensor:
     r = r.reshape(r.shape + (1,) * x.ndim)
 
     return reduce(x - r, "... r x -> ... x", "prod")
+
+
+def unwrap(x: torch.Tensor) -> torch.Tensor:
+    y: torch.Tensor = x % (2 * pi)
+
+    return torch.where(y > pi, 2 * pi - y, y)
