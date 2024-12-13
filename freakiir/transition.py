0000c46818a7317734aa42e171b468fb8220e828
Jacob Koziej <jacobkoziej@gmail.com>
2024-12-13 00:37:43 -0500

    Define `zplane2horn_torus()`

--- freakiir/transition.py
+++ freakiir/transition.py
@@ -2,3 +2,17 @@
 #
 # transition.py -- transition maps
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+import torch
+
+from torch import pi
+
+
+def zplane2horn_torus(z: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    theta: torch.Tensor = z.angle()
+
+    phi: torch.Tensor
+    phi = 2 * (pi / 2 + torch.arctan(torch.log(z.abs())))
+    phi = torch.where(phi <= pi, phi, -(2 * pi - phi))
+
+    return theta, phi
